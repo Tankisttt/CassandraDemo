83974a29c151f83f44e7f94b81c2ac4b306ec9c3
Roman Bachurin <Roman.Bachurin@waveaccess.ru>
2022-05-07 22:52:02 +0500

    Fixed warnings

--- CassandraDemo/User.cs
+++ CassandraDemo/User.cs
@@ -1,18 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 
+#pragma warning disable CS8618
+
 namespace CassandraDemo;
 
 public class User
 {
     public Guid Id { get; set; }
+
     public string Email { get; set; }
 
     public string FirstName { get; set; }
 
     public string LastName { get; set; }
 
-    [Range(0, 125)]
-    public int Age { get; set; }
+    [Range(0, 125)] public int Age { get; set; }
 
     public string? Country { get; set; }
 }
